28c5e08c1c612cb27adf327a1beb3b0323d91b12
Matt Murfitt <murfitt@gmail.com>
2014-04-19 23:30:27 -0700

    Writes dm3 files from nparray

--- parse_dm3_grammar.py
+++ parse_dm3_grammar.py
@@ -33,7 +33,7 @@
 # first one tried will write it, and subsequent will allow it as a valid
 # possibility
 dm3_grammar = """
-header:     version(>l)=3, len(>l), _pos=f.tell(), endianness(>l)=1, section, len=f.tell()-_pos
+header:     version(>l)=3, len(>l), _pos=f.tell(), endianness(>l)=1, section, len=f.tell()-_pos, zero_pad_0(>l)=0, zero_pad_1(>l)=0
 section:    is_dict(b), open(b), num_tags(>l), data(["named_data"]*num_tags)
 named_data: sdtype(b)=20, name_length(>H), name({name_length}s), section
 named_data: sdtype(b)=21, name_length(>H), name({name_length}s), dataheader
@@ -537,10 +537,26 @@ def dict_to_dm3(d):
     # needed for complex data types? - in which case we should find a better
     # way of converting it in dm3_to_dictionary)
     ret = {}
+    # we convert simple python types to a small subset of DM types
     dm_types = {float: 7,
                 int: 3,
                 long: 5,
                 bool: 8}
+    # but we need to convert array types to the proper type.
+    # this should mirror the simple types in the grammar
+    # (could this be extracted from the grammar itself?)
+    """simpledata_2 = h
+        simpledata_3 = i
+        simpledata_4 = H
+        simpledata_5 = I
+        simpledata_6 = f
+        simpledata_7 = d
+        simpledata_8 = b
+        simpledata_9 = b
+        simpledata_10 = b
+        simpledata_11 = q
+        simpledata_12 = Q"""
+    struct_types = dict(h=2, i=3, H=4, I=5, f=6, d=7, b=8, q=11, Q=12)
 
     def data_to_dataheader(data):
         ret = dict()
@@ -552,7 +568,7 @@ def dict_to_dm3(d):
             ret['struct_data'] = dict(data=data)
         elif isinstance(data, array):
             ret['dtype'] = 20
-            ret['array_data'] = dict(arraydtype=dm_types[type(data[0])],
+            ret['array_data'] = dict(arraydtype=struct_types[data.typecode],
                                      len=len(data),
                                      array=data)
         else:
@@ -613,7 +629,7 @@ if __name__ == '__main__':
     assert len(out['section'].data[0].dataheader.struct_data.data) == 4
     assert out['section'].data[0].name == 'ApplicationBounds'
 
-    write_also = False
+    write_also = True
     if write_also:
         #log.setLevel(logging.DEBUG)
         with open("out".join(os.path.splitext(fname)), 'wb') as outf:
@@ -627,7 +643,29 @@ if __name__ == '__main__':
         bob=3, arg=3.151)))
     print test_tags
     with open('test_tags.gtg', 'wb') as outf2:
-        log.setLevel(logging.DEBUG)
+        #log.setLevel(logging.DEBUG)
         g.save(outf2, test_tags)
-    # would now need to convert to a nicer dict, prefereably one compatible with parse_dm3.
 
+    with open('test_tags.gtg', 'rb') as inf2:
+        in_test_tags = g.open(inf2)
+        print dm3_to_dictionary(in_test_tags)
+    # if we want to save an image, need ndarray_to_imagedatadict from
+    # dm3_image_utils
+    from dm3_image_utils import ndarray_to_imagedatadict
+    import numpy as np
+    z=np.random.random((512,512))
+    z[10:100, 20:80] = 0.5
+    with open('test.dm3', 'wb') as outdm3:
+        image = ndarray_to_imagedatadict(z)
+        ret = {}
+        ret["ImageList"] = [{"ImageData": image}]
+        # I think ImageSource list creates a mapping between ImageSourceIds and Images
+        ret["ImageSourceList"] = [{"ClassName": array('H', "ImageSource:Simple".encode('utf_16_le')), "Id": [0], "ImageRef": 0}]
+        # I think this lists the sources for the DocumentObjectlist. The source number is not
+        # the indxe in the imagelist but is either the index in the ImageSourceList or the Id
+        # from that list. We also need to set the annotation type to identify it as an image
+        ret["DocumentObjectList"] = [{"ImageSource": 0, "AnnotationType": 20}]
+        # finally some display options
+        ret["Image Behavior"] = {"ViewDisplayID": 8}
+        ret["InImageMode"] = 1
+        g.save(outdm3, dict_to_dm3(ret))
